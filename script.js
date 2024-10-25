ce01ea8f9ee14470dc6f8fae4557347efae8fa04
Ahmed <shaikahmed3070@gmail.com>
2024-10-25 22:58:21 +0530

    updated

--- script.js
+++ script.js
@@ -17,4 +17,19 @@ const testimonials = [
         image:"profile.png",
         testimonial:`Dedicated and motivated final year Computer Science Student. Seeking opportunities to apply and enhance my technical skills, academic knowledge and gain hands-on experience while contributing and gaining skills under mentorship of experienced professionals.`,
     },
-]
+];
+const testimonialContainer = document.getElementById("_testimonial_container");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+let currentIndex = 0;
+
+function showTestimonial() {
+
+}
+
+function changeTestimonial() {
+
+}
+
+prevBtn.addEventListener("click",()=> changeTestimonial(-1));
+nextBtn.addEventListener("click",()=> changeTestimonial(1));
