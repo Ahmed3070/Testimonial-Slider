313196959ab9d7f9891be23d31e2339e246b1fa8
Ahmed <shaikahmed3070@gmail.com>
2024-10-25 22:52:05 +0530

    updated

---
+++ script.js
@@ -0,0 +1,20 @@
+const testimonials = [
+    {
+        name:"Shaik Ahmed",
+        position:"Associate Software Engineer",
+        image:"profile.png",
+        testimonial:`Dedicated and motivated final year Computer Science Student. Seeking opportunities to apply and enhance my technical skills, academic knowledge and gain hands-on experience while contributing and gaining skills under mentorship of experienced professionals.`,
+    },
+    {
+        name:"Shaik Ahmed",
+        position:"Associate Software Engineer",
+        image:"profile.png",
+        testimonial:`Dedicated and motivated final year Computer Science Student. Seeking opportunities to apply and enhance my technical skills, academic knowledge and gain hands-on experience while contributing and gaining skills under mentorship of experienced professionals.`,
+    },
+    {
+        name:"Shaik Ahmed",
+        position:"Associate Software Engineer",
+        image:"profile.png",
+        testimonial:`Dedicated and motivated final year Computer Science Student. Seeking opportunities to apply and enhance my technical skills, academic knowledge and gain hands-on experience while contributing and gaining skills under mentorship of experienced professionals.`,
+    },
+]
